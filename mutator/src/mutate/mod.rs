812d73d0200add9057a9dc258cd0502b9e64d1d7
Aurèle Oulès <aurele@oules.com>
2022-12-16 19:45:13 +0100

    Ignore src/wallet/test

--- mutator/src/mutate/mod.rs
+++ mutator/src/mutate/mod.rs
@@ -166,6 +166,7 @@ pub fn generate_mutations_from_pr(pr_number: i64) -> Vec<Mutation> {
         if file_path.starts_with("test/")
             || file_path.starts_with("src/test")
             || file_path.starts_with("src/qt/test")
+            || file_path.starts_with("src/wallet/test")
             || file_path.starts_with("doc")
         {
             continue;
